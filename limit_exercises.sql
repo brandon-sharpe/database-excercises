783a36de2d84c1a519e19a213f9844193e4f73c9
brandon-sharpe <brandon.robert.sharpe@gmail.com>
2021-06-27 15:24:25 -0500

    Limit practice excercise using the employees database from the Codeup servers

---
+++ limit_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+DESCRIBE titles;
+
+SELECT DISTINCT title FROM titles;
+
+DESCRIBE employees;
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY hire_date
+LIMIT 5; 
+
+/* Alselm	Cappello	
+Utz	Mandell	
+Bouchung	Schreiter	
+Baocai	Kushner	
+Petter	Stroustrup*/
+
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY hire_date
+LIMIT 5 OFFSET 50; 
+
+
+
+
+
+
+
