26ef016160ec28574d1e4cf5c0b4f4ef5ccb15af
brandon-sharpe <brandon.robert.sharpe@gmail.com>
2021-07-01 16:00:43 -0500

    1-3

--- case_excercise.sql
+++ case_excercise.sql
@@ -12,38 +12,21 @@ WHERE to_date = (SELECT MAX(to_date) FROM dept_emp)) dnum using (emp_no)
 GROUP BY emp_no;
 
 -- 2. Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name. 
-SELECT 
-	CONCAT(first_name," ",last_name), last_name,
+SELECT first_name, last_name,
 	CASE 
-		WHEN last_name LIKE 'A%' THEN 'A-H'
-		WHEN last_name LIKE 'B%' THEN 'A-H'
-		WHEN last_name LIKE 'C%' THEN 'A-H'
-		 WHEN last_name LIKE 'D%' THEN 'A-H'
-		 WHEN last_name LIKE 'E%' THEN 'A-H'
-		 WHEN last_name LIKE 'F%' THEN 'A-H'
-		 WHEN last_name LIKE 'G%' THEN 'A-H'
-		 WHEN last_name LIKE 'H%' THEN 'A-H'
-		 WHEN last_name LIKE 'I%' THEN 'I-Q'
-		 WHEN last_name LIKE 'J%' THEN 'I-Q'
-		 WHEN last_name LIKE 'K%' THEN 'I-Q'
-		 WHEN last_name LIKE 'L%' THEN 'I-Q'
-		 WHEN last_name LIKE 'M%' THEN 'I-Q'
-		 WHEN last_name LIKE 'N%' THEN 'I-Q'
-		 WHEN last_name LIKE 'O%' THEN 'I-Q'
-		 WHEN last_name LIKE 'P%' THEN 'I-Q'
-		 WHEN last_name LIKE 'Q%' THEN 'I-Q'
-		 WHEN last_name LIKE 'R%' THEN 'R-Z'
-		 WHEN last_name LIKE 'S%' THEN 'R-Z'
-		 WHEN last_name LIKE 'T%' THEN 'R-Z'
-		 WHEN last_name LIKE 'U%' THEN 'R-Z'
-		 WHEN last_name LIKE 'V%' THEN 'R-Z'
-		 WHEN last_name LIKE 'W%' THEN 'R-Z'
-		 WHEN last_name LIKE 'X%' THEN 'R-Z'
-		 WHEN last_name LIKE 'Y%' THEN 'R-Z'
-		 WHEN last_name LIKE 'Z%' THEN 'R-Z'
-		 ELSE 'Other'
-		END AS alpha_group
+		WHEN SUBSTR(last_name, 1, 1) BETWEEN 'A' and 'H' THEN 'A-H' 
+		WHEN SUBSTR(last_name, 1, 1) BETWEEN 'I' and 'Q' THEN 'I-Q'
+		WHEN SUBSTR(last_name, 1, 1) BETWEEN 'R' and 'Z' THEN 'R-Z'
+	END AS 'alpha_group'
 FROM employees;
 
+-- 3 
+SELECT
+	COUNT(CASE WHEN birth_date LIKE '195%' THEN birth_date ELSE NULL END) AS "Born in the 50's",
+	COUNT(CASE WHEN birth_date LIKE '196%' THEN birth_date ELSE NULL END) AS "Born in the 60's",
+	COUNT(CASE WHEN birth_date LIKE '197%' THEN birth_date ELSE NULL END) AS "Born in the 70's",
+	COUNT(CASE WHEN birth_date LIKE '198%' THEN birth_date ELSE NULL END) AS "Born in the 80's",
+	COUNT(CASE WHEN birth_date LIKE '199%' THEN birth_date ELSE NULL END) AS "Born in the 90's"
+FROM employees;
 
 	
